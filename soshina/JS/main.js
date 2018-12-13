949753b762758e8e29fcebbf1f9289cbd31e86f6
IGO(いぐお) <43200655+IGO-kon@users.noreply.github.com>
2018-12-13 16:54:52 +0900

    Add files via upload

--- soshina/JS/main.js
+++ soshina/JS/main.js
@@ -16,7 +16,7 @@
 
   ref = firebase.storage().ref().child('img/sample.jpg');
 ref.getDownloadURL().then((url) => {
-  document.getElementById('image').src = url;
+  document.getElementById('embed').src = url;
 });
 
 
@@ -33,7 +33,7 @@ var imgSample = storageRef.child('sample.png');
 window.onload = function() {
     //htmlロード完了したらストレージの画像を表示してみる
     imgSample.getDownloadURL().then(function(url){
-      document.getElementById("imgSample").style.backgroundImage = "url("+url+")";
+      document.getElementById("embed").style.backgroundImage = "url("+url+")";
 
 
     }).catch(function(error) {
@@ -43,12 +43,35 @@ window.onload = function() {
     });
   };
 
-//   console.log(url);
 
-  var btnUploadChange = function(ev){
-      var zzz=ev.target.files[0].name;
-      console.log(zzz);
+//アップロード処理
+
+  
+//時刻データを取得して変数jikanに格納する
+var jikan = new Date();
+
+//時・分・秒を取得する
+var hour = jikan.getHours();
+var minute = jikan.getMinutes();
+var second = jikan.getSeconds();
+
+console.log (jikan);
+
+//ランダム数
+
+  var rand = Math.random();
+
+  console.log(rand);
+
+
+var btnUploadChange = function(ev){
+ var zzz=ev.target.files[0].name;
+    
     //ストレージへアップロードするファイルのパスを生成する
+  var zzz =jikan + rand +zzz;
+
+    console.log(zzz);
+
     var uploadRef = storageRef.child(zzz);
     const f = ev.target.files[0];
     uploadRef.put(f).then(function(snapshot) {
